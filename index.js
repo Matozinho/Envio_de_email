6660b87e327e3b751243ee6ea6bcbf1902b1dda4
Matozinho <felipi.matozinho@unioeste.br>
2021-04-16 19:39:58 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const { sendMail } = require("./sendEmail.js");
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello Wolrd");
+});
+
+app.get("/sendEmail", (req, res) => {
+  const { name, phone } = req.query;
+
+  sendMail(name, phone)
+    .then((status) => {
+      res.sendStatus(status);
+    })
+    .catch((status) => {
+      res.sendStatus(status);
+    });
+});
+
+app.listen(3000, () => console.log(`Started server at http://localhost:3000!`));
---
+++ sendEmail.js
@@ -0,0 +1,39 @@
+const nodemailer = require("nodemailer");
+const SMTP_CONFIG = require("./config/smtp");
+
+const transporter = nodemailer.createTransport({
+  host: SMTP_CONFIG.host,
+  port: SMTP_CONFIG.port,
+  secure: false,
+  auth: {
+    user: SMTP_CONFIG.user,
+    pass: SMTP_CONFIG.pass,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+module.exports.sendMail = (name, phone) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (name && phone && phone.length == 11) {
+        const mailSent = await transporter
+          .sendMail({
+            text: `
+          name: ${name}
+          phone: ${phone}
+          `,
+            subject: `LEAD!`,
+            from: `"Oportunidade no Interior <${process.env.GMAIL_USER}>"`,
+            to: process.env.EMAIL_DESTINY,
+          })
+          .catch((err) => reject(500));
+
+        resolve(200);
+      } else throw new Error("Inputs preenchidos incorretamente!");
+    } catch (err) {
+      reject(400);
+    }
+  });
+};
